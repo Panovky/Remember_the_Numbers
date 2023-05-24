afdb97137d7d14332a9b77ecd768d0e220246ffb
Ksenia Panova <rybinak95@gmail.com>
2023-05-24 15:32:10 +0300

    correction

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import sqlite3 as sl3
 import os
-
-from sqlalchemy import MetaData
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopmentConfig')
 
-metadata = MetaData(
-    naming_convention={
-        "ix": 'ix_%(column_0_label)s',
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s"
-    }
-)
-
-db = SQLAlchemy(app, metadata=metadata)
-migrate = Migrate(app, db, render_as_batch=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
